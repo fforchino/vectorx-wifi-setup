544644cac0ced52c856f0aebc38c936703c96371
frcfppud <filippo.forchino@gmail.com>
2023-01-25 18:48:42 +0100

    Adapt to VectorX

--- wifi-reset.sh
+++ wifi-reset.sh
@@ -8,5 +8,5 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" >/etc/wpa_supplicant/wpa_supplicant.conf
-echo "update_config=1" >/etc/wpa_supplicant/wpa_supplicant.conf
+echo "update_config=1" >>/etc/wpa_supplicant/wpa_supplicant.conf
 echo "DONE"
