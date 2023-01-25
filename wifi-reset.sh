1bb2285e4c6062900bae26a1e9c78df8bc4057d0
frcfppud <filippo.forchino@gmail.com>
2023-01-25 18:47:36 +0100

    Adding reset script

---
+++ wifi-reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root. sudo ./wifi-reset.sh"
+    exit 1
+fi
+
+echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev" >/etc/wpa_supplicant/wpa_supplicant.conf
+echo "update_config=1" >/etc/wpa_supplicant/wpa_supplicant.conf
+echo "DONE"
